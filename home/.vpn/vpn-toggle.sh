c5bf7a9f7a1c38893f002a59d8a6474f1b1f2c57
Cypher-Monarch <monarchcypher@gmail.com>
2026-04-20 10:09:53 +0530

    chore(dotfiles): sync latest config changes

--- home/.vpn/vpn-toggle.sh
+++ home/.vpn/vpn-toggle.sh
@@ -6,7 +6,7 @@ if pgrep -x "openvpn" >/dev/null; then
   pkexec killall openvpn
   pkexec /usr/bin/sysctl -w net.ipv6.conf.all.disable_ipv6=0
 else
-  if pgrep -x "cyphergate.elf" >/dev/null; then
+  if pgrep -x "cyphergate" >/dev/null; then
     notify-send "You've got cyphergate running" "Open it!"
   else
     gtk-launch CypherGate.desktop
