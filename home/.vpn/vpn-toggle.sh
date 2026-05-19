7d09ffbdab2ced4357048c795b77e390c863646e
Cypher-Monarch <monarchcypher@gmail.com>
2026-05-19 12:36:01 +0530

    chore(dotfiles): sync latest config changes

--- home/.vpn/vpn-toggle.sh
+++ home/.vpn/vpn-toggle.sh
@@ -3,8 +3,12 @@
 VPN_PROC_NAME="openvpn"
 
 if pgrep -x "openvpn" >/dev/null; then
-  pkexec killall openvpn
-  pkexec /usr/bin/sysctl -w net.ipv6.conf.all.disable_ipv6=0
+  python -c "import json, socket                     
+  sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+  sock.connect('/tmp/cyphergate-root-handler.sock')
+  sock.send(json.dumps({'action':'STOP_VPN'}).encode())
+  sock.send(json.dumps({'action':'ENABLE_IPV6'}).encode())
+  sock.close()"
 else
   if pgrep -x "cyphergate" >/dev/null; then
     notify-send "You've got cyphergate running" "Open it!"
