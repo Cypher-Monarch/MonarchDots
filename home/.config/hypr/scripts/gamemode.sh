7fcc03ed442ce40068b397d59a13c989274251a6
Cypher-Monarch <monarchcypher@gmail.com>
2026-06-21 21:38:35 +0530

    chore(dotfiles): sync latest config changes

--- home/.config/hypr/scripts/gamemode.sh
+++ home/.config/hypr/scripts/gamemode.sh
@@ -8,18 +8,26 @@
 #
 
 if [ -f $HOME/.config/misc/settings/gamemode-enabled ]; then
-    hyprctl reload
-    rm $HOME/.config/misc/settings/gamemode-enabled
-    notify-send "Gamemode deactivated" "Animations and blur enabled"
+  hyprctl reload
+  rm $HOME/.config/misc/settings/gamemode-enabled
+  notify-send "Gamemode deactivated" "Animations and blur enabled"
 else
-    hyprctl --batch "\
-        keyword animations:enabled 0;\
-        keyword decoration:shadow:enabled 0;\
-        keyword decoration:blur:enabled 0;\
-        keyword general:gaps_in 0;\
-        keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0"
-    touch $HOME/.config/misc/settings/gamemode-enabled
-    notify-send "Gamemode activated" "Animations and blur disabled"
+  hyprctl eval '
+    hl.config({
+        animations = {
+            enabled = false,
+        },
+        decoration = {
+            rounding = 0,
+            shadow = { enabled = false },
+            blur = { enabled = false },
+        },
+        general = {
+            gaps_in = 0,
+            gaps_out = 0,
+            border_size = 1,
+        },
+    })'
+  touch $HOME/.config/misc/settings/gamemode-enabled
+  notify-send "Gamemode activated" "Animations and blur disabled"
 fi
