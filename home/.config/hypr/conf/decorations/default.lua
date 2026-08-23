f2f2ec36a183afee48056f7d76e82c8192d9e97c
Cypher-Monarch <monarchcypher@gmail.com>
2026-08-23 20:57:30 +0530

    chore(dotfiles): sync latest config changes

--- home/.config/hypr/conf/decorations/default.lua
+++ home/.config/hypr/conf/decorations/default.lua
@@ -3,28 +3,25 @@
 -- name: "Default"
 -- -----------------------------------------------------
 
-
 hl.config({
-    decoration = {
-        rounding = 5,
-        active_opacity = 0.8,
-        inactive_opacity = 0.6,
-        fullscreen_opacity = 1.0,
-        blur = {
-            enabled = true,
-            size = 6,
-            passes = 2,
-            new_optimizations = true,
-            ignore_opacity = true,
-            xray = true,
-            -- blurls = waybar
-        },
-        shadow = {
-            enabled = true,
-            range = 30,
-            render_power = 3,
-            color = 0x66000000,
-        },
-    },
+	decoration = {
+		rounding = 5,
+		active_opacity = 0.8,
+		inactive_opacity = 0.6,
+		fullscreen_opacity = 1.0,
+		blur = {
+			enabled = true,
+			size = 6,
+			passes = 2,
+			new_optimizations = true,
+			ignore_opacity = true,
+			xray = true,
+		},
+		shadow = {
+			enabled = true,
+			range = 30,
+			render_power = 3,
+			color = 0x66000000,
+		},
+	},
 })
-
